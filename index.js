675e88b524178ce23daa1c49e70ff256f65c4140
Damien Seguin <dmnsgn@gmail.com>
2024-03-11 11:30:04 +0000

    refactor: rename temp variable and cellCount

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 import { avec3, vec3 } from "pex-math";
 
-const TEMP_0 = vec3.create();
-const TEMP_1 = vec3.create();
-const TEMP_2 = vec3.create();
+const TEMP_CELL = vec3.create();
+const TEMP_VEC3_1 = vec3.create();
+const TEMP_VEC3_2 = vec3.create();
 
 function normals(positions, cells, normals) {
   const isFlatArray = !positions[0]?.length;
   const isCellsFlatArray = !cells[0]?.length;
-  const l = cells.length / (isCellsFlatArray ? 3 : 1);
+  const cellCount = cells.length / (isCellsFlatArray ? 3 : 1);
 
   if (isFlatArray) {
     normals ||= new positions.constructor(positions.length).fill(0);
 
-    for (let fi = 0; fi < l; fi++) {
+    for (let fi = 0; fi < cellCount; fi++) {
       if (isCellsFlatArray) {
-        avec3.set(TEMP_0, 0, cells, fi);
+        avec3.set(TEMP_CELL, 0, cells, fi);
       } else {
-        vec3.set(TEMP_0, cells[fi]);
+        vec3.set(TEMP_CELL, cells[fi]);
       }
 
-      avec3.set(TEMP_1, 0, positions, TEMP_0[1]); // b
-      avec3.sub(TEMP_1, 0, positions, TEMP_0[0]); // ab = b - a
-      avec3.normalize(TEMP_1, 0);
+      avec3.set(TEMP_VEC3_1, 0, positions, TEMP_CELL[1]); // b
+      avec3.sub(TEMP_VEC3_1, 0, positions, TEMP_CELL[0]); // ab = b - a
+      avec3.normalize(TEMP_VEC3_1, 0);
 
-      avec3.set(TEMP_2, 0, positions, TEMP_0[2]); // c
-      avec3.sub(TEMP_2, 0, positions, TEMP_0[0]); // ac = c - a
-      avec3.normalize(TEMP_2, 0);
+      avec3.set(TEMP_VEC3_2, 0, positions, TEMP_CELL[2]); // c
+      avec3.sub(TEMP_VEC3_2, 0, positions, TEMP_CELL[0]); // ac = c - a
+      avec3.normalize(TEMP_VEC3_2, 0);
 
-      avec3.cross(TEMP_1, 0, TEMP_2, 0); // ab x ac
-      avec3.normalize(TEMP_1, 0);
+      avec3.cross(TEMP_VEC3_1, 0, TEMP_VEC3_2, 0); // ab x ac
+      avec3.normalize(TEMP_VEC3_1, 0);
 
       for (let i = 0; i < 3; i++) {
-        avec3.add(normals, TEMP_0[i], TEMP_1, 0);
+        avec3.add(normals, TEMP_CELL[i], TEMP_VEC3_1, 0);
       }
     }
 
@@ -48,22 +48,22 @@ function normals(positions, cells, normals) {
 
   normals ||= [];
 
-  for (let fi = 0; fi < l; fi++) {
+  for (let fi = 0; fi < cellCount; fi++) {
     if (isCellsFlatArray) {
-      avec3.set(TEMP_0, 0, cells, fi);
+      avec3.set(TEMP_CELL, 0, cells, fi);
     } else {
-      vec3.set(TEMP_0, cells[fi]);
+      vec3.set(TEMP_CELL, cells[fi]);
     }
 
-    const a = positions[TEMP_0[0]];
+    const a = positions[TEMP_CELL[0]];
 
-    vec3.normalize(vec3.sub(vec3.set(TEMP_1, positions[TEMP_0[1]]), a)); // ab = b - a
-    vec3.normalize(vec3.sub(vec3.set(TEMP_2, positions[TEMP_0[2]]), a)); // ac = c - a
-    vec3.normalize(vec3.cross(TEMP_1, TEMP_2)); // ab x ac
+    vec3.normalize(vec3.sub(vec3.set(TEMP_VEC3_1, positions[TEMP_CELL[1]]), a)); // ab = b - a
+    vec3.normalize(vec3.sub(vec3.set(TEMP_VEC3_2, positions[TEMP_CELL[2]]), a)); // ac = c - a
+    vec3.normalize(vec3.cross(TEMP_VEC3_1, TEMP_VEC3_2)); // ab x ac
 
     for (let i = 0; i < 3; i++) {
-      normals[TEMP_0[i]] ||= [0, 0, 0];
-      vec3.add(normals[TEMP_0[i]], TEMP_1);
+      normals[TEMP_CELL[i]] ||= [0, 0, 0];
+      vec3.add(normals[TEMP_CELL[i]], TEMP_VEC3_1);
     }
   }
 
